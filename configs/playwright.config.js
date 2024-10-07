eb025c76e77b04e0ff25c6458248da0c61c07f4e
omnisoho <omnisoho@gmail.com>
2024-10-07 20:16:53 +0800

    update playwright config timeout

--- configs/playwright.config.js
+++ configs/playwright.config.js
@@ -61,11 +61,13 @@ module.exports = defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'PORT=3001 npm run start',
-    // command: 'set PORT=3001 && npm run start',
+    // command: 'PORT=3001 npm run start',
+    command: 'set PORT=3001 && npm run start',
     url: 'http://127.0.0.1:3001',
     reuseExistingServer: !process.env.CI,
   },
+
+  timeout: 60000,
 
   globalSetup: require.resolve('./playwright-global-setup'),
 });
